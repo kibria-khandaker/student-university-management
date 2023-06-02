534380ba570cf8d0400070dc0d68887647612186
Golam Kibria <kibriakhandaker@gmail.com>
2023-06-02 16:24:44 +0600

    SUM-3 create .env and exp, mongs, cors, parser, install

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
-console.log("Hi I am Server Ts");
+import mongoose from "mongoose";
+import config from "./config/index";
+import app from "./app";
+
+async function bootstrap() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log(` : Database Connected`);
+        app.listen(config.port, () => {
+            console.log(` : App listening on port ${config.port}`)
+        })
+
+    } catch (error) {
+        console.log("Failed to Database", error);
+    }
+}
+bootstrap()
+
+
+
