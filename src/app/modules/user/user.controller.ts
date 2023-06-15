b9c6a09e11d7a6c4ce50cb69e4ba01969c804332
Golam Kibria <kibriakhandaker@gmail.com>
2023-06-15 12:19:06 +0600

    Before Optimize Again everything Ok.

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,28 +1,18 @@
-import { NextFunction, Request, Response } from 'express'
+import { RequestHandler } from 'express'
 import { UserService } from './user.service'
-import catchAsync from '../../../shared/catchAsync'
-import sendResponse from '../../../shared/sendResponse'
-import httpStatus from 'http-status'
 
-const createUser = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
+const createUser: RequestHandler = async (req, res, next) => {
+  try {
     const { user } = req.body
     const result = await UserService.createUser(user)
-
-    next()
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
+    res.status(200).json({
       success: true,
       message: 'User created successfully',
       data: result,
     })
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'User created successfully',
-    //   data: result,
-    // })
+  } catch (err) {
+    next(err)
   }
-)
+}
 
 export const UserController = { createUser }
