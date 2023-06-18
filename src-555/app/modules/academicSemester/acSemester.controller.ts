f4ac856df6c3eec7aa5478af20a53d7854f3e8c9
Golam Kibria <kibriakhandaker@gmail.com>
2023-06-18 20:08:09 +0600

    before catchAsync

---
+++ src-555/app/modules/academicSemester/acSemester.controller.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from 'express'
+import { AcSemesterService } from './acSemester.service'
+import catchAsync from '../../../shared/catchAsync'
+import sendResponse from '../../../shared/sendResponse'
+import httpStatus from 'http-status'
+import { IAcSemester } from './acSemester.interface'
+
+const createSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { ...acSemesterData } = req.body
+    const result = await AcSemesterService.createSemester(acSemesterData)
+    // next()
+    sendResponse<IAcSemester>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Semester created successfully',
+      data: result,
+    })
+    next()
+  }
+)
+export const AcSemesterController = { createSemester }
+
+/*
+import { RequestHandler } from 'express'
+import { AcSemesterService } from './acSemester.service'
+
+const createSemester: RequestHandler = async (req, res, next) => {
+  try {
+    const { ...acSemesterData } = req.body
+    const result = await AcSemesterService.createSemester(acSemesterData)
+    res.status(200).json({
+      success: true,
+      message: 'Semester created successfully',
+      data: result,
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const AcSemesterController = { createSemester }
+*/
