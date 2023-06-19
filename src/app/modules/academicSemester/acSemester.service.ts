ce67d38c6983493a94cc43eeef6bec4836d443d9
Golam Kibria <kibriakhandaker@gmail.com>
2023-06-19 19:41:53 +0600

    [Done]:Make dynamic searching and filtering and CRUD Api

--- src/app/modules/academicSemester/acSemester.service.ts
+++ src/app/modules/academicSemester/acSemester.service.ts
@@ -1,7 +1,10 @@
 import httpStatus from 'http-status'
 import ApiError from '../../../errors/ApiError'
-import { acSemesterTitleCodeMapper } from './acSemester.constant'
-import { IAcSemester } from './acSemester.interface'
+import {
+  acSemesterSearchableFields,
+  acSemesterTitleCodeMapper,
+} from './acSemester.constant'
+import { IAcSemester, IAcSemesterFilter } from './acSemester.interface'
 import { AcSemester } from './acSemester.model'
 import { IPaginationOptions } from '../../../interfaces/pagination'
 import { IGenericResponse } from '../../../interfaces/common'
@@ -17,8 +20,56 @@ const createSemester = async (payload: IAcSemester): Promise<IAcSemester> => {
 }
 
 const getAllSemesters = async (
+  filters: IAcSemesterFilter,
   paginationOptions: IPaginationOptions
 ): Promise<IGenericResponse<IAcSemester[]>> => {
+  const { searchTerm, ...filtersData } = filters
+
+  const andConditions = []
+
+  if (searchTerm) {
+    andConditions.push({
+      $or: acSemesterSearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    })
+  }
+
+  console.log(Object.keys(filtersData))
+  if (Object.keys(filtersData).length) {
+    andConditions.push({
+      $and: Object.entries(filtersData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    })
+  }
+
+  // const andConditions = [
+  //   {
+  //     $or: [
+  //       {
+  //         title: {
+  //           $regex: searchTerm,
+  //           $option: 'i',
+  //         },
+  //       },{
+  //         code:{
+  //           $regex: searchTerm,
+  //           $option: 'i',
+  //         }
+  //       },{
+  //         year:{
+  //           $regex: searchTerm,
+  //           $option: 'i',
+  //         }
+  //       }
+  //     ],
+  //   },
+  // ]
+
   const { page, limit, skip, sortBy, sortOrder } =
     paginationHelper.calculatePagination(paginationOptions)
 
@@ -27,9 +78,10 @@ const getAllSemesters = async (
     sortConditions[sortBy] = sortOrder
   }
 
-  const result = await AcSemester.find()
-    // .sort({ createdAt: 'desc' })
-    // .sort({ year: 'desc' })
+  const whereConditions =
+    andConditions.length > 0 ? { $and: andConditions } : {}
+
+  const result = await AcSemester.find(whereConditions)
     .sort(sortConditions)
     .skip(skip)
     .limit(limit)
@@ -46,7 +98,38 @@ const getAllSemesters = async (
   }
 }
 
+const getSingleSemester = async (id: string): Promise<IAcSemester | null> => {
+  const result = await AcSemester.findById(id)
+  return result
+}
+
+const updateSemester = async (
+  id: string,
+  payload: Partial<IAcSemester>
+): Promise<IAcSemester | null> => {
+  if (
+    payload.title &&
+    payload.code &&
+    acSemesterTitleCodeMapper[payload.title] !== payload.code
+  ) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Semester Code')
+  }
+
+  const result = await AcSemester.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  })
+  return result
+}
+
+const deleteSemester = async (id: string): Promise<IAcSemester | null> => {
+  const result = await AcSemester.findByIdAndDelete(id)
+  return result
+}
+
 export const AcSemesterService = {
   createSemester,
   getAllSemesters,
+  getSingleSemester,
+  updateSemester,
+  deleteSemester,
 }
